6cbd16216e6f47ab2877e5c81b315b86a0362307
Oshoke <oyatioshoke@yahoo.com>
2022-05-28 00:35:44 +0100

    space

--- src/Components/Hero.js
+++ src/Components/Hero.js
@@ -29,7 +29,7 @@ const Hero = function () {
                 solutions for digital products. 🚀
               </p>
 
-              <div className='flex gap-x-5'>
+              <div className='flex space-x-4'>
                 <a
                   href='https://twitter.com/Airshokes'
                   target='_blank'
