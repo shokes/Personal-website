cab6f7266d4672d24c083375506b81a05c2e96d6
Oshoke <oyatioshoke@yahoo.com>
2022-06-09 09:33:24 +0100

    message desired

--- src/Components/SingleWork.js
+++ src/Components/SingleWork.js
@@ -31,7 +31,7 @@ const SingleWork = function ({ title, desc, mainImage, id }) {
             className='w-[29rem] h-[30rem] lg:h-[35rem] project-image rounded-lg'
           />
 
-          <span className='absolute top-1/3 font-semibold right-1/2 text-lg see-more translate-y-[1.5rem] translate-x-1/2 bg-white  p-14 w-5 h-5 flex items-center justify-center rounded-full text-black'>
+          <span className='absolute top-1/3 font-semibold right-1/2  md:right-[27rem] lg:right-1/2 text-lg see-more translate-y-[1.5rem] translate-x-1/2 bg-white  p-14 w-5 h-5 flex items-center justify-center rounded-full text-black'>
             See more
           </span>
         </Link>
